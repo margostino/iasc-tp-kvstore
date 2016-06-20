f530bf45171d3f73a5dc7b60847964d75710df46
duneding <maj.dagostino@gmail.com>
2016-06-20 17:54:44 -0300

    add test

--- kvstore/test/kvstore_test.exs
+++ kvstore/test/kvstore_test.exs
@@ -13,7 +13,15 @@ defmodule KVStoreTest do
     {:ok, worker_pid} = Supervisor.start_child(sup_pid, [:server])
     assert KVStore.put(:server, 1, "value1") == {:error, ["Key must be String"]}
     assert KVStore.put(:server, "key1", 3232) == {:error, ["Value must be String"]}
-    assert KVStore.put(:server, 212, 3232) == {:error, ["Value must be String", "Key must be String"]}    
+    assert KVStore.put(:server, 212, 3232) == {:error, ["Value must be String", "Key must be String"]}
+    assert KVStore.put(:server, 212, 3232) == {:error, ["Value must be String", "Key must be String"]}
+  end
+
+  test "validate compare values" do
+    {:ok, sup_pid} = KVStore.Supervisor.start_link
+    {:ok, worker_pid} = Supervisor.start_child(sup_pid, [:server])
+    assert KVStore.put(:server, "k1", "b") == :ok
+    assert KVStore.filter(:server, "gt", "a") == {:ok, ["b"]}
   end
 
   test "broke supervised worker" do
