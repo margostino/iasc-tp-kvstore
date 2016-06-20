70c9a151667496955bd6c906a3b93ef5261fbe35
duneding <maj.dagostino@gmail.com>
2016-06-20 16:06:02 -0300

    Add validate and test

--- kvstore/lib/kv_store.ex
+++ kvstore/lib/kv_store.ex
@@ -16,17 +16,11 @@ defmodule KVStore do
   end
 
   def put(server, key, value) do
-    if !(is_bitstring(key) && is_bitstring(value)) do
-      {:error, "Key/Value must be String"}
-    else
-      key_size = Application.get_env(:kvstore, :key_size)
-      value_size = Application.get_env(:kvstore, :value_size)
-      if (String.length(key) <= key_size && String.length(value) <= value_size) do
+    case (validate(key, value)) do
+      {:error, reasons} -> {:error, reasons}
+      :ok ->
         GenServer.cast(server, {:put, key, value})
-      else
-        {:error, "Key/Value wrong size"}
-      end
-    end
+    end    
   end
 
   def delete(server, key) do
@@ -67,4 +61,59 @@ defmodule KVStore do
     IO.puts "Message not understood :("
     {:noreply, state}
   end
+
+  ## Private
+
+  defp validateType(val) do
+    if !(is_bitstring(val)) do
+      :error
+    else
+      :ok
+    end
+  end
+
+  defp validateSize(val, size) do
+    if !(String.length(val) <= size) do
+      :error
+    else
+      :ok
+    end
+  end
+
+  defp validate(key, value) do
+    errors = []
+    key_size = Application.get_env(:kvstore, :key_size)
+    value_size = Application.get_env(:kvstore, :value_size)
+
+    #Validate KEY
+    case validateType(key) do
+      :error ->
+        errors = Enum.into(errors, ["Key must be String"])
+      :ok ->
+        case validateSize(key, key_size) do
+          :error ->
+            errors = Enum.into(errors, ["Key wrong size. Size must be #{key_size}"])
+          :ok -> "nothing"
+        end
+    end
+
+    #Validate VALUE
+    case validateType(value) do
+      :error ->
+        errors = Enum.into(errors, ["Value must be String"])
+      :ok ->
+        case validateSize(value, value_size) do
+          :error ->
+            errors = Enum.into(errors, ["Value wrong size. Size must be #{value_size}"])
+          :ok -> "nothing"
+        end
+    end
+
+    if (Enum.count(errors)>0) do
+      {:error, errors}
+    else
+      :ok
+    end
+  end
+
 end
--- kvstore/test/kvstore_test.exs
+++ kvstore/test/kvstore_test.exs
@@ -2,11 +2,18 @@ defmodule KVStoreTest do
   use ExUnit.Case
 
   test "start supervised worker and put/get value" do
-    value = %{name: "martin", age: 30}
     {:ok, sup_pid} = KVStore.Supervisor.start_link
     {:ok, worker_pid} = Supervisor.start_child(sup_pid, [:server])
-    assert KVStore.put(:server, 1, value) == :ok
-    assert KVStore.get(:server, 1) == {:ok, value}
+    assert KVStore.put(:server, "key1", "value1") == :ok
+    assert KVStore.get(:server, "key1") == {:ok, "value1"}
+  end
+
+  test "validate data type and size KV" do
+    {:ok, sup_pid} = KVStore.Supervisor.start_link
+    {:ok, worker_pid} = Supervisor.start_child(sup_pid, [:server])
+    assert KVStore.put(:server, 1, "value1") == {:error, ["Key must be String"]}
+    assert KVStore.put(:server, "key1", 3232) == {:error, ["Value must be String"]}
+    assert KVStore.put(:server, 212, 3232) == {:error, ["Value must be String", "Key must be String"]}    
   end
 
   test "broke supervised worker" do
