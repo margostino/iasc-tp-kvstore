9478c16ce71905847b90045445a26e5c22129780
Alejandro D'Ambrosio <alejandro.dambrosio@gmail.com>
2016-07-08 14:10:00 -0300

    Separación de nodos - final

--- kvstore/lib/kv_api.ex
+++ kvstore/lib/kv_api.ex
@@ -9,72 +9,55 @@ defmodule KVStore.Api do
 
   ## Client API
   def get(key) do
-    GenServer.call({:global, __MODULE__}, {:get, key})
+    GenServer.call({:global, KVStore.Data}, {:get, key})
   end
 
   def keys() do
-    GenServer.call({:global, __MODULE__}, {:keys})
+    GenServer.call({:global, KVStore.Data}, {:keys})
   end
 
   def values() do
-    GenServer.call({:global, __MODULE__}, {:values})
+    GenServer.call({:global, KVStore.Data}, {:values})
   end
 
-  def put(key, value) do
-    case (validate(key, value)) do
+"""
+     case (validate(key, value)) do
       {:error, reasons} -> {:error, reasons}
       :ok ->
-        GenServer.cast({:global, __MODULE__}, {:put, key, value})
+"""
+"""
     end
+"""
+
+  def put(key, value) do
+        GenServer.cast({:global, KVStore.Data}, {:put, key, value})
   end
 
   def delete(key) do
-    GenServer.cast({:global, __MODULE__}, {:delete, key})
+    GenServer.cast({:global, KVStore.Data}, {:delete, key})
   end
 
   def break() do
-    Process.exit({:global, __MODULE__}, :shutdown)
+    Process.exit({:global, KVStore.Data}, :shutdown)
   end
 
-  # Operators: gt, lt, gte, lte
-  def filter(operator, value) do
-    operator = String.downcase(operator)
+
+"""
     case (validate(operator)) do
       {:error, reasons} -> {:error, reasons}
       :ok ->
-        GenServer.call({:global, __MODULE__}, {:filter, value, operator})
+"""
+"""
     end
-  end
+"""
 
-  ## Server Callbacks ----------------------------------------------------------
-  #-----------------------------------------------------------------------------
-
-  def handle_call({:get, key}, _from, state) do
-    {:reply, {:ok, KVStore.Data.get(key)}, state}
-  end
-
-  def handle_call({:keys}, _from, state) do
-    {:reply, {:ok, KVStore.Data.keys()}, state}
-  end
-
-  def handle_call({:values}, _from, state) do
-    {:reply, {:ok, KVStore.Data.values()}, state}
-  end
-
-  def handle_cast({:put, key, value}, state) do
-    {:noreply, KVStore.Data.put(key, value)}
-  end
-
-  def handle_cast({:delete, key}, state) do
-    {:noreply, KVStore.Data.delete(key)}
-  end
-
-  def handle_call({:filter, value, operator}, _from, state) do
-    {:reply, {:ok, KVStore.Data.filter(value, operator)}, state}
+  def filter(operator, value) do
+    operator = String.downcase(operator)
+        GenServer.call({:global, KVStore.Data}, {:filter, value, operator})
   end
 
   def handle_info(msg, state) do
-    IO.puts "Message not understood :("
+    IO.puts "Message not understood :( #{inspect msg}"
     {:noreply, state}
   end
 
