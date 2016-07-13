d150232ee3f1d97c6241eb5ba4047572f14c740a
Alejandro D'Ambrosio <alejandro.dambrosio@gmail.com>
2016-07-13 11:16:07 -0300

    Revert "Aplicado ETS en data nodes - parcial"
    This reverts commit f6bd8d671be1854d535114ccdf57fafc94031410.

--- kvstore/lib/kv_data.ex
+++ kvstore/lib/kv_data.ex
@@ -1,7 +1,4 @@
 defmodule KVStore.Data do
-  @@moduledoc """
-  Nodo de datos para la KVStore
-  """
   use GenServer
   require Logger
 
@@ -10,73 +7,41 @@ defmodule KVStore.Data do
     GenServer.start_link(__MODULE__, [], name: {:global, __MODULE__})
   end
 
-  def init(table) do
-    # 3. We have replaced the names map by the ETS table
-    names = :ets.new(:data_table, [:named_table, :private, read_concurrency: true])
-    refs  = %{}
-    {:ok, {names, refs}}
-  end
-
   ## Client API
   def handle_call({:get, key}, _from, state) do
     Logger.info "GET: #{key}"
-    case :ets.member(:data_table, key) do
-      true -> {:reply, {:ok, Map.get(map, key)}, state}
-      false -> {:reply, {:ok, :not_found}, state}
+    map = Enum.into(state, %{})
+    if Map.has_key?(map, key) do
+      {:reply, {:ok, Map.get(map, key)}, state}
+    else
+      {:reply, {:ok, :not_found}, state}
     end
   end
 
-  @doc """
-  Obtiene la lista de claves guardadas
-  """
   def handle_call({:keys}, _from, state) do
-    firstKey = :ets.first(:data_table)
-    {:reply, {:ok, firstKey, keys(firstKey, [firstKey])}, state}
+    map = Enum.into(state, %{})
+    {:reply, {:ok, Map.keys(map)}, state}
   end
 
-  keys('$end_of_table', ['$end_of_table'|keysResult]) ->
-      keysResult;
-
-  keys(currKey, keysResult) ->
-      nextKey = :ets.next(:data_table, currKey),
-      keys(nextKey, [nextKey|keysResult])
-
-
-  @doc """
-  Obtiene la lista de valores guardados
-  """
   def handle_call({:values}, _from, state) do
-    firstKey = :ets.first(:data_table)
-    {:reply, {:ok, :ets.lookup(:data_table, firstKey), values(firstKey, [:ets.lookup(:data_table, firstKey)])}, state}
+    map = Enum.into(state, %{})
+    {:reply, {:ok, Map.values(map)}, state}
   end
 
-    values('$end_of_table', ['$end_of_table'|valuesResult]) ->
-        valuesResult;
-
-    values(currKey, valuesResult) ->
-        :ets.lookup(:data_table, (nextKey = :ets.next(:data_table, currKey))),
-        keys(nextKey, [:ets.lookup(:data_table, nextKey)|valuesResult]).
-
-  @doc """
-  Agrega (o reemplaza si ya existe) un valor dada una clave
-  """
   def handle_cast({:put, key, value}, state) do
     Logger.info "PUT: #{key},#{value}"
-    {:noreply, :ets.insert(:data_table, key, value)}
+    map = Enum.into(state, %{})
+    #{:noreply, Enum.into([], ["#{key}": value])}
+    {:noreply, Enum.into([], Map.to_list(Map.put(map, key, value)))}
   end
 
-  @doc """
-  Borra un valor a partir de la clave
-  """
   def handle_cast({:delete, key}, state) do
-    {:noreply, :ets.delete(:data_table, key)}
+    {:noreply, Map.delete(Enum.into(state, %{}), key)}
   end
 
-  @doc """
-  Obtiene los valores que cumplen la condición de valor de referencia y de operador
-  """
   def handle_call({:filter, value, operator}, _from, state) do
-    results = :ets.select(:data_table, :ets.fun2ms(fn(x) -> compare().(x, value, operator) end))
+    values = Map.values(Enum.into(state, %{}))
+    results = Enum.filter(values, fn(x) -> compare().(x, value, operator) end)
     {:reply, {:ok, results}, state}
   end
 
